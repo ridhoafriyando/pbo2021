515eac31fdddff017f10e87c1042d98ecbb738b0
ridhoafriyando <78814296+ridhoafriyando@users.noreply.github.com>
2021-03-31 22:22:35 +0700

    Praktikum 05 Autoloading & Namespace

---
+++ application/Dosen.php
@@ -0,0 +1,31 @@
+<?php
+    namespace application;
+
+    class Dosen extends Pegawai
+    {
+        public $nidn;
+        public $jabatan_akademis;
+
+        function __construct($nip, $nama, $hp, $gaji, $nidn, $jabatan){
+            $this->nip = $nip;
+            $this->nama = $nama;
+            $this->no_hp = $hp;
+            $this->gaji_pokok = $gaji;
+            $this->nidn = $nidn;
+            $this->jabatan_akademis = $jabatan;
+        }
+
+        public function mengajar(){
+
+        }
+        public function meneliti(){
+
+        }
+
+        public function getNama()
+        {
+            return $this->nama;
+        }
+    }
+
+?> 
