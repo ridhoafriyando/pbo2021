acbd668ed431af3042c50ab52fe3a1d3b03aaa15
ridhoafriyando <78814296+ridhoafriyando@users.noreply.github.com>
2021-05-01 20:21:51 +0700

    Add files via upload

--- application/MahasiswaBaru.php
+++ application/MahasiswaBaru.php
@@ -1,20 +1,75 @@
-<?php 
-    namespace application;
+<?php
 
-    class MahasiswaBaru extends Mahasiswa
-    {
-        public $no_registrasi;
+namespace App;
 
-        function __construct($nim, $nama, $tgl, $jk, $regis){
-            $this->nim = $nim;
-            $this->nama = $nama;
-            $this->tanggal_lahir = $tgl;
-            $this->jenis_kelamin = $jk;
-            $this->no_registrasi = $regis;
-        }
+class MahasiswaBaru extends Mahasiswa{
+    protected $no_registrasi;
 
-        public function bayarGedung(){
-            echo "$this->username Anda Telah Membayar Gedung";
-        }
+    function __construct($nim,$nama,$tgl,$jk,$no_reg){
+        $this->nim = $nim;
+        $this->nama = $nama;
+        $this->tanggal_lahir = $tgl;
+        $this->jenis_kelamin = $jk;
+        $this->no_registrasi = $no_reg;
     }
-?> 
+
+    function bayarGedung(){
+
+    }
+
+    public function getNim()
+    {
+        return $this->nim;
+    }
+
+    public function getNama()
+    {
+        return $this->nama;
+    }
+
+    public function getTanggalLahir()
+    {
+        return $this->tanggal_lahir;
+    }
+
+    public function getJenisKelamin()
+    {
+        return $this->jenis_kelamin;
+    }
+
+    public function getNoReg()
+    {
+        return $this->no_registrasi;
+    }
+
+    public function setNim($nim)
+    {
+        $this->nim = $nim;
+    }
+
+    public function setNama($nama)
+    {
+        $this->nama = $nama;
+    }
+
+    public function setTanggalLahir($tgl)
+    {
+        $this->tanggal_lahir = $tgl;
+    }
+
+    public function setJenisKelamin($jk)
+    {
+        $this->jenis_kelamin = $jk;
+    }
+
+    public function setNoReg($no_reg)
+    {
+        $this->no_registrasi = $no_reg;
+    }
+
+    // public function tuntaskan() //error karena tuntaskan merupakan function di class Mahasiswa adalah final
+    // {
+    //     echo "bawa perbaikan";
+    // }
+}
+?>
